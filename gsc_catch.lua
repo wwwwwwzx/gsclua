d088f36af565771cffca837156dbb3d9fabd59c2
wwwwwwzx <togekiss.beyond@gmail.com>
2017-09-12 22:33:45 -0700

    Update catching bot for all versions

--- gsc_catch.lua
+++ gsc_catch.lua
@@ -1,15 +1,12 @@
 local catch_flag
 local delay = 1000
 local version = memory.readword(0x14e)
-if version == 0xae0d or version == 0x2d68 then
-    print("USA Gold/Silver detected")
-    catch_flag = 0xc028
-elseif version == 0x6084 or version == 0x341d then
-    print("Japanese Gold/Silver detected")
-elseif version == 0xd218 or version == 0xe2f2 then
-    print("USA/Europe Crystal detected")
-elseif version == 0x409a then
-    print("Japanese Crystal detected")
+if version == 0xae0d or version == 0x2d68 or version == 0x6084 or version == 0x341d  then
+    print("Gold/Silver detected")
+    catch_flag = 0xc00a
+elseif version == 0xd218 or version == 0xe2f2 or version == 0x409a then
+    print("Crystal detected")
+    catch_flag = 0xc10a
 else
     print(string.format("Unknown version, code: %4x", version))
     print("Script stopped")
@@ -23,7 +20,7 @@ while true do
     for i = 1, delay do
         emu.frameadvance()
     end
-    if memory.readword(catch_flag) == 0x01 then
+    if memory.readword(catch_flag) ~= 0 then
         print("Gotcha!!")
         break
     else
