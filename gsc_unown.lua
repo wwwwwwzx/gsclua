206964753777e8b99db82de5549d1a7bc961fdb2
Real96 <chicco.gaeta@gmail.com>
2017-09-21 23:23:47 +0200

    Update gsc_egg.lua (#5)
    * Update gsc_egg.lua
    
    Added japanese addresses and better game version/region checks
    
    * Update gsc_fish.lua
    
    * Update gsc_gift.lua
    
    * Update gsc_headbutt.lua
    
    * Update gsc_stationary.lua
    
    * Update gsc_unown.lua
    
    * Update gsc_wild.lua

--- gsc_unown.lua
+++ gsc_unown.lua
@@ -6,19 +6,41 @@ local atkdef
 local spespc
 
 local enemy_addr
-local version = memory.readword(0x14e)
-if version == 0xae0d or version == 0x2d68 then
-    print("USA Gold/Silver detected")
-    enemy_addr = 0xd0f5
-elseif version == 0x6084 or version == 0x341d then
-    print("Japanese Gold/Silver detected")
-    enemy_addr = 0xd0e7
-elseif version == 0xd218 or version == 0xe2f2 then
-    print("USA/Europe Crystal detected")
-    enemy_addr = 0xd20c
-elseif version == 0x409a then
-    print("Japanese Crystal detected")
-    enemy_addr = 0xd23d
+local version = memory.readbyte(0x141)
+local region = memory.readbyte(0x142)
+if version == 0x54 then
+	if region == 0x44 or region == 0x46 or region == 0x49 or region == 0x53 then
+		print("EUR Crystal detected")
+		enemy_addr = 0xd20c
+	elseif region == 0x45 then
+		print("USA Crystal detected")
+		enemy_addr = 0xd20c
+	elseif region == 0x4A then
+		print("JAP Crystal detected")
+		enemy_addr = 0xd23d
+	end
+elseif version == 0x55 then
+	if region == 0x44 or region == 0x46 or region == 0x49 or region == 0x53 then
+		print("EUR Gold detected")
+		enemy_addr = 0xd0f5
+	elseif region == 0x45 then
+		print("USA Gold detected")
+		enemy_addr = 0xd0f5
+	elseif region == 0x4A then
+		print("JAP Gold detected")
+		enemy_addr = 0xd0e7
+	end
+elseif version == 0x58 then
+	if region == 0x44 or region == 0x46 or region == 0x49 or region == 0x53 then
+		print("EUR Silver detected")
+		enemy_addr = 0xd0f5
+	elseif region == 0x45 then
+		print("USA Silver detected")
+		enemy_addr = 0xd0f5
+	elseif region == 0x4A then
+		print("JAP Silver detected")
+		enemy_addr = 0xd0e7
+	end
 else
     print(string.format("Unknown version, code: %4x", version))
     print("Script stopped")
