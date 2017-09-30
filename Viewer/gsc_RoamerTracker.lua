38e08555bd7cc772668705fc3a3e0487ec0dd106
Real96 <chicco.gaeta@gmail.com>
2017-09-30 15:53:00 +0200

    Update gsc_RoamerTracker.lua

--- Viewer/gsc_RoamerTracker.lua
+++ Viewer/gsc_RoamerTracker.lua
@@ -1,106 +1,84 @@
--- Edit the party slot you wanna track
-overlayslot = 1
----
 
-naturestr={
-     "Hardy","Lonely","Brave","Adamant","Naughty",
-     "Bold","Docile","Relaxed","Impish","Lax",
-     "Timid","Hasty","Serious","Jolly","Naive",
-     "Modest","Mild","Quiet","Bashful","Rash",
-     "Calm","Gentle","Sassy","Careful","Quirky"}
 
-local base_address
+beastsname = {"Raikou","Entei","Suicune"}
+pos = 0
+
+local beasts_addr = 0xffff
 local version = memory.readbyte(0x141)
 local region = memory.readbyte(0x142)
 if version == 0x54 then
-	if region == 0x44 or region == 0x46 or region == 0x49 or region == 0x53 then
-		print("EUR Crystal detected")
-		base_address = 0xdcd7
-	elseif region == 0x45 then
-		print("USA Crystal detected")
-		base_address = 0xdcd7
-	elseif region == 0x4A then
-		print("JPN Crystal detected")
-		base_address = 0xdc9d
-	end
+    if region == 0x44 or region == 0x46 or region == 0x49 or region == 0x53 then
+        print("EUR Crystal detected")
+        beasts_addr = 0xdfcf
+    elseif region == 0x45 then
+        print("USA Crystal detected")
+        beasts_addr = 0xdfcf
+    elseif region == 0x4A then
+        print("JPN Crystal detected")
+        beasts_addr = 0xdf45
+    end
 elseif version == 0x55 or version == 0x58 then
-	if region == 0x44 or region == 0x46 or region == 0x49 or region == 0x53 then
-		print("EUR Gold/Silver detected")
-		base_address = 0xda22
-	elseif region == 0x45 then
-		print("USA Gold/Silver detected")
-		base_address = 0xda22
-	elseif region == 0x4A then
-		print("JPN Gold/Silver detected")
-		base_address = 0xd9e8
+    if region == 0x44 or region == 0x46 or region == 0x49 or region == 0x53 then
+        print("EUR Gold/Silver detected")
+        beasts_addr = 0xdd1a
+    elseif region == 0x45 then
+        print("USA Gold/Silver detected")
+        beasts_addr = 0xdd1a
+    elseif region == 0x4A then
+        print("JPN Gold/Silver detected")
+        beasts_addr = 0xdc90
     elseif region == 0x4B then
         print("KOR Gold/Silver detected")
-        base_address = 0xdb1f
-	end
-else
+        beasts_addr = 0xde17
+    end
+end
+if beasts_addr == 0xffff then
     print(string.format("Unknown version, code: %4x", version))
     print("Script stopped")
-    return
 end
 
--- Check PKHex for PK2 structure
-function parsePKM(address)
-    local exp = Readbytes(address + 0x8, 3)
-    local atkdef = memory.readbyte(address + 0x15)
-    local spespc = memory.readbyte(address + 0x16)
-    local atk = math.floor(atkdef/16)
-    local def = atkdef%16
-    local spe = math.floor(spespc/16)
-    local spc = spespc%16
-    local hp = atk % 2 * 8 + def % 2 * 4 + spe % 2 * 2 + spc % 2
-    print(string.format("Species: %3d\tNature:\t", memory.readbyte(address))..naturestr[exp % 25 + 1].."\t"..shiny(atkdef,spespc))
-    print(string.format("Exp: %d\tFriendShip: %d",exp,memory.readbyte(address + 0x1b)))
-    print(string.format("\tHP\tAtk\tDef\tSpe\tSpc\t"))
-    print(string.format("IVs:\t%d\t%d\t%d\t%d\t%d", hp, atk, def, spe, spc ))
-    print(string.format("EVs:\t%d\t%d\t%d\t%d\t%d", Readbytes(address + 0xB,2), Readbytes(address + 0xD,2), Readbytes(address + 0xF,2), Readbytes(address + 0x11,2), Readbytes(address + 0x13,2)))
-end
-
-defaultfont = {r = 0xFF, g = 0xFF, b = 0xFF, a = 0x90};
-function ShowPKM(address)
-    local exp = Readbytes(address + 0x8, 3)
-    local atkdef = memory.readbyte(address + 0x15)
-    local spespc = memory.readbyte(address + 0x16)
-    local atk = math.floor(atkdef/16)
-    local def = atkdef%16
-    local spe = math.floor(spespc/16)
-    local spc = spespc%16
-    local hp = atk % 2 * 8 + def % 2 * 4 + spe % 2 * 2 + spc % 2
-    gui.text(2,0,string.format("Species: %3d\tNature:\t", memory.readbyte(address))..naturestr[exp % 25 + 1].."\t"..shiny(atkdef,spespc),defaultfont)
-    gui.text(2,10,string.format("Exp: %d\tFriendShip: %d",exp,memory.readbyte(address + 0x1b)),defaultfont)
-    gui.text(2,20,string.format("        HP   Atk   Def   Spe   Spc"),defaultfont)
-    gui.text(2,30,string.format("IVs:%6d%6d%6d%6d%6d", hp, atk, def, spe, spc ),defaultfont)
-    gui.text(2,40,string.format("EVs:%6d%6d%6d%6d%6d", Readbytes(address + 0xB,2), Readbytes(address + 0xD,2), Readbytes(address + 0xF,2), Readbytes(address + 0x11,2), Readbytes(address + 0x13,2)),defaultfont)
-end
-
-function Readbytes(address,n)
-    local data = 0
-    for i = 0,n-1 do
-        data = 0x100 * data + memory.readbyte(address + i)
-    end
-    return data
-end
-
+defaultfont= {r = 0xFF, g = 0xFF, b = 0xFF, a = 0x90};
 function shiny(atkdef,spespc)
     if spespc == 0xAA then
         if atkdef == 0x2A or atkdef == 0x3A or atkdef == 0x6A or atkdef == 0x7A or atkdef == 0xAA or atkdef == 0xBA or atkdef == 0xEA or atkdef == 0xFA then
-            return "Shiny!!"
+            return {r = 0x0, g = 0x0, b = 0xFF, a = 0x70}
         end
     end
-    return ""
+    return defaultfont
 end
 
-local partysize = memory.readbyte(base_address)
-for i = 0,partysize - 1 do
-    print ("Party "..i+1)
-    parsePKM(base_address + 0x08 + i * 0x30)
+-- species, lv ,location ,location, HP, DV1, DV2
+function parsebeast(address,idx)
+    species = memory.readbyte(address)
+    if species == 243 or species == 244 or species == 245 then
+        local HP = memory.readbyte(address + 0x4)
+        local atkdef = memory.readbyte(address + 0x5)
+        local spespc = memory.readbyte(address + 0x6)
+        local atk = math.floor(atkdef/16)
+        local def = atkdef%16
+        local spe = math.floor(spespc/16)
+        local spc = spespc%16
+        color = shiny(atkdef,spespc)
+        gui.text(2, pos , beastsname[species - 242].."\tLocation "..memory.readbyte(address + 0x2)..memory.readbyte(address + 0x3)..string.format("\tCurrent HP: %d ", HP), color)
+        pos = pos + 10
+        if HP == 0 then
+            gui.text(2, pos,"Never Encounter", defaultfont)
+            pos = pos + 10
+            return
+        end
+        local hp = atk % 2 * 8 + def % 2 * 4 + spe % 2 * 2 + spc % 2
+        gui.text(2, pos, string.format("IVs: HP:%2d Atk:%2d Def:%2d Spe:%2d Spc:%2d", hp, atk, def, spe, spc ),color)
+        pos = pos + 10
+    else 
+        gui.text(2, pos, beastsname[idx + 1].."\tNot Avaliable", defaultfont)
+        pos = pos + 10
+    end
 end
+
 while true do
-    ShowPKM(base_address + 0x08 + (overlayslot - 1) * 0x30)
-    gui.text(2,133,'Slot: '..overlayslot)
+    pos = 2
+    for i = 0,2 do
+        parsebeast(beasts_addr + 0x7 * i, i)
+    end
     emu.frameadvance()
 end
